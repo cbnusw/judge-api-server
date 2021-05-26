e1bf2b192ff3486d62792270b7fd047e7176b52c
DongWooE <ehddn977@naver.com>
2021-05-26 20:43:51 +0900

    Fetch Latest Commits 이전에 consumer가 처음부터 실행되었을시에 모든 메세지를 출력하는 문제를 consumer가 소비하지않은 commit들부터 재소비하도록 바꿈

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -4,7 +4,7 @@ const kafkaConsumer = function () {
   const topic = 'submit';     //나중에 result로 바꿔야합니다
   const client = new KafkaClient({ kafkaHost: 'localhost:9092' });
   const topics = [{ topic: topic, partition: 0 }];
-  const options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
+  const options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024, autoCommit : true };
   const consumer = new Consumer(client, topics, options);
 
   consumer.on('message', (message) => {
